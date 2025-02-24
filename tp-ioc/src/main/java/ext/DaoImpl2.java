508cb99081381e79089d5edb18f70b4eb4edccaf
achraf <achraf.tata2004@gmail.com>
2025-02-24 17:39:52 +0000

    injection dynamique version XML

---
+++ tp-ioc/src/main/java/ext/DaoImpl2.java
@@ -0,0 +1,11 @@
+package ext;
+
+import dao.IDao;
+
+public class DaoImpl2 implements IDao {
+    public double gitData() {
+        System.out.println("version web service");
+        double temp = 30;
+        return temp;
+    }
+}
